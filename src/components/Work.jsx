22c68960db80bf0b602070667c397995794b8982
Lipsit Pradhan <lipsitpradhan72110@gmail.com>
2024-03-03 19:39:11 +0530

    made changes in navbar and work components

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -23,7 +23,7 @@ const Work = () => {
             {/* Hover Effects*/}
             <div className=" opacity-0 group-hover:opacity-100">
               <span className=" text-2xl font-bold text-white tracking-wider">
-                RecipeVault
+                RecipeStore
               </span>
               <div className=" pt-8 text-center">
                 {/* <a href="/">
@@ -45,7 +45,7 @@ const Work = () => {
             {/* Hover Effects*/}
             <div className=" opacity-0 group-hover:opacity-100">
               <span className=" text-2xl font-bold text-white tracking-wider">
-                Headline-Hub
+                News Headline
               </span>
               <div className=" pt-8 text-center">
                 {/* <a href="/">
@@ -67,7 +67,7 @@ const Work = () => {
             {/* Hover Effects*/}
             <div className=" opacity-0 group-hover:opacity-100">
               <span className=" text-2xl font-bold text-white tracking-wider">
-                InstaChat
+                ChatApp
               </span>
               <div className=" pt-8 text-center">
                 {/* <a href="/">
